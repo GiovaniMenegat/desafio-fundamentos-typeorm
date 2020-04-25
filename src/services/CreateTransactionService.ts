bfce6b00b02bdbc2807be502e0ba1cafa16ae205
GiovaniMenegat <gmenegatsynapps@gmail.com>
2020-04-25 12:37:49 -0300

    transaction and category POST

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,49 @@
 // import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_title: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_title,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid type.');
+    }
+
+    let category = await categoryRepository.findOne({
+      where: { title: category_title },
+    });
+
+    if (!category) {
+      categoryRepository.create({ title: category_title });
+      category = await categoryRepository.save({ title: category_title });
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+    await transactionsRepository.save(transaction);
+
+    return { ...transaction, category };
   }
 }
 
